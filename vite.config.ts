b39e3d1d9f7dbb4f8f9723e2acd42f64a0aad115
efek0349 <kndmrefe@gmail.com>
2025-08-02 21:05:05 +0300

    pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-<<<<<<< HEAD
-// https://vitejs.dev/config/
 export default defineConfig({
-=======
-export default defineConfig({
-  base: '/mesaitakip-web/',
->>>>>>> a258f08 (pages)
+  base: '/mesaitakip/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
