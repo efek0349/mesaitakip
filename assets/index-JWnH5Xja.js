e96706e41ef8d54f38779a7fc7d8b9dd9fdacbf0
efek0349 <kndmrefe@gmail.com>
2026-03-19 18:34:11 +0300

    Updates

---
+++ assets/index-JWnH5Xja.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Bexot_jw.js","assets/index-BuMkyJ50.js","assets/index-FFmm7Kmm.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-BuMkyJ50.js";const _=p("App",{web:()=>r(()=>import("./web-Bexot_jw.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
