3da2a63bbf9ee062762713676d6d8e5cf6563f9a
efek0349 <kndmrefe@gmail.com>
2026-04-12 14:58:05 +0300

    Updates

---
+++ assets/index-Cc_hiuaT.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-B7AvInnJ.js","assets/index-3aP8lDE2.js","assets/index-Ce1cL7Xc.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-3aP8lDE2.js";const _=p("App",{web:()=>r(()=>import("./web-B7AvInnJ.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
