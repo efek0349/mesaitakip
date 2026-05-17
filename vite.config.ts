23bcc86b5de7d7efeebd42d9994f6f4ff1e0eb04
efek0349 <kndmrefe@gmail.com>
2026-05-17 20:49:56 +0300

    up vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-oxc';
 
 export default defineConfig({
-  base: '/',
+  base: '/mesaitakip/',
   plugins: [react()],
   server: { host: 'localhost', port: 3000 },
   build: { outDir: 'dist' },
