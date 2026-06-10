234dfe884885c85aa71638249c6a92ee2afa0baf
efek0349 <kndmrefe@gmail.com>
2026-06-10 04:45:02 +0300

    Updates

--- assets/esm-LSnUYMnl.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-y_Bbj39f.js","assets/index-Bp88sjon.js","assets/index-7O6StM3f.css"])))=>i.map(i=>d[i]);
-import{a as e,n as t}from"./index-Bp88sjon.js";var n=e(`App`,{web:()=>t(()=>import(`./web-y_Bbj39f.js`).then(e=>new e.AppWeb),__vite__mapDeps([0,1,2]))});export{n as App};
