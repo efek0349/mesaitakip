fc0315c58e9717700e35225c00b1c515b73c3588
efek0349 <kndmrefe@gmail.com>
2026-06-11 22:45:41 +0300

    Updates

--- assets/esm-BYUOD2BL.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DCKAeB1f.js","assets/index-DrnDuLcX.js","assets/index-7O6StM3f.css"])))=>i.map(i=>d[i]);
-import{a as e,n as t}from"./index-DrnDuLcX.js";var n=e(`App`,{web:()=>t(()=>import(`./web-DCKAeB1f.js`).then(e=>new e.AppWeb),__vite__mapDeps([0,1,2]))});export{n as App};
